c70a9bc25063ddea7e2523a6fa94a9e25a39912f
Denis Sancov <denis.sancov@gmail.com>
2021-04-20 17:35:29 +0300

    improved date time format

--- utils/src/main/java/md/sancov/utils/ex/DateTimeFormatEx.kt
+++ utils/src/main/java/md/sancov/utils/ex/DateTimeFormatEx.kt
@@ -1,12 +1,23 @@
 package md.sancov.utils.ex
 
 import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+inline fun<reified T> DateTimeFormatter.tryParse(text: CharSequence): T? {
+    return try {
+        val accessor = parse(text)
 
-fun DateTimeFormatter.tryParse(text: CharSequence): Instant? {
-    return try{
-        Instant.from(parse(text))
+        val result = when (T::class) {
+            Instant::class -> Instant.from(accessor)
+            LocalDate::class -> LocalDate.from(accessor)
+            LocalDateTime::class -> LocalDateTime.from(accessor)
+            else -> null
+        }
+
+        result as? T
+
     } catch (_: Throwable) {
         null
     }
