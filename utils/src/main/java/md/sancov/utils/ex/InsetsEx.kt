f5af5f5a5eb8034f9edfda28bd59f15e39a772a7
Denis Sancov <denis.sancov@gmail.com>
2021-04-20 17:40:15 +0300

    cleanup

--- utils/src/main/java/md/sancov/utils/ex/InsetsEx.kt
+++ utils/src/main/java/md/sancov/utils/ex/InsetsEx.kt
@@ -3,10 +3,10 @@ package md.sancov.utils.ex
 import androidx.core.graphics.Insets
 
 fun Insets.update(
-        left: Int = this.left,
-        top: Int = this.top,
-        right: Int = this.right,
-        bottom: Int = this.bottom
+    left: Int = this.left,
+    top: Int = this.top,
+    right: Int = this.right,
+    bottom: Int = this.bottom
 ): Insets {
     return Insets.of(left, top, right, bottom)
 }
--- utils/src/main/java/md/sancov/utils/ex/InstantEx.kt
+++
@@ -1,8 +0,0 @@
-package md.sancov.utils.ex
-
-import java.time.Instant
-import java.time.temporal.ChronoUnit
-
-val Instant.dropTime: Instant get() {
-    return truncatedTo(ChronoUnit.DAYS)
-}
