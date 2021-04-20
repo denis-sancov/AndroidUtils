b9ee1b430d12bb74e34a4a918d16c177f3c3effd
Denis Sancov <denis.sancov@gmail.com>
2021-04-20 16:35:34 +0300

    various improvements

--- utils/src/main/java/md/sancov/utils/model/Text.kt
+++ utils/src/main/java/md/sancov/utils/model/Text.kt
@@ -6,15 +6,23 @@ import androidx.annotation.StringRes
 import kotlinx.parcelize.Parcelize
 
 sealed class Text: Parcelable {
+    abstract override fun equals(other: Any?): Boolean
+    abstract override fun hashCode(): Int
+
     @Parcelize
     data class Resource(@StringRes val resourceId: Int, val multiline: Boolean = false): Text()
 
     @Parcelize
-    data class Chars(val sequence: CharSequence, val multiline: Boolean = false): Text()
+    data class Chars(val sequence: CharSequence?, val multiline: Boolean = false): Text()
 
     val isMultiline: Boolean get() = when (this) {
         is Resource -> multiline
         is Chars -> multiline
+    }
+
+    val isEmpty: Boolean get() = when (this) {
+        is Resource -> resourceId <= 0
+        is Chars -> sequence.isNullOrBlank()
     }
 
     fun resolve(ctx: Context): String? {
