3ffbe590935f34581f6e071d3e9685a5c931d427
Denis Sancov <denis.sancov@gmail.com>
2021-04-21 11:17:09 +0300

    resolve Text using Lambda expression

--- utils/src/main/java/md/sancov/utils/model/Img.kt
+++ utils/src/main/java/md/sancov/utils/model/Img.kt
@@ -1,8 +1,5 @@
 package md.sancov.utils.model
 
-import android.os.Parcelable
 import androidx.annotation.DrawableRes
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-data class Img(@DrawableRes val resourceId: Int): Parcelable
+data class Img(@DrawableRes val resourceId: Int)
--- utils/src/main/java/md/sancov/utils/model/Text.kt
+++ utils/src/main/java/md/sancov/utils/model/Text.kt
@@ -5,30 +5,33 @@ import android.os.Parcelable
 import androidx.annotation.StringRes
 import kotlinx.parcelize.Parcelize
 
-sealed class Text: Parcelable {
+sealed class Text {
     abstract override fun equals(other: Any?): Boolean
     abstract override fun hashCode(): Int
 
-    @Parcelize
     data class Resource(@StringRes val resourceId: Int, val multiline: Boolean = false): Text()
 
-    @Parcelize
     data class Chars(val sequence: CharSequence?, val multiline: Boolean = false): Text()
+
+    data class Lambda(val lambda: (Context) -> CharSequence?, val multiline: Boolean = false): Text()
 
     val isMultiline: Boolean get() = when (this) {
         is Resource -> multiline
         is Chars -> multiline
+        is Lambda -> multiline
     }
 
     val isEmpty: Boolean get() = when (this) {
         is Resource -> resourceId <= 0
         is Chars -> sequence.isNullOrBlank()
+        is Lambda -> false
     }
 
     fun resolve(ctx: Context): String? {
         return when (this) {
             is Resource -> if (resourceId == 0) null else ctx.getString(resourceId)
             is Chars -> sequence.toString()
+            is Lambda -> lambda(ctx).toString()
         }
     }
 }
