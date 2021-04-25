4cde6e8df3218ea67671f0b8b03743d7e21b53f3
Denis Sancov <denis.sancov@gmail.com>
2021-04-25 15:05:26 +0300

    suspend attribute to mapState new method - mapStateNotNull

--- utils/src/main/java/md/sancov/utils/ex/FlowEx.kt
+++ utils/src/main/java/md/sancov/utils/ex/FlowEx.kt
@@ -2,8 +2,17 @@ package md.sancov.utils.ex
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 import md.sancov.utils.State
 
-inline fun <T, reified R> Flow<State<T>>.mapState(crossinline transform: (value: T?) -> R): Flow<State<R>> {
+suspend inline fun <T, reified R> Flow<State<T>>.mapState(
+        crossinline transform: (value: T?) -> R
+): Flow<State<R>> {
     return map { it.map(transform) }
+}
+
+suspend inline fun <T, reified R> Flow<State<T>>.mapNotNullState(
+        crossinline transform: (value: T?) -> R
+): Flow<State<R>> {
+    return mapNotNull { it.map(transform) }
 }
